0eb98604adc43d918d9ec1abc53fcc96cbab8af1
thanhhd@aladintech.co <thanhhd@aladintech.co>
2024-06-04 21:12:55 +0700

    fix padding mobile

--- src/components/common/StarpxHeader.vue
+++ src/components/common/StarpxHeader.vue
@@ -8,6 +8,6 @@ const { account, handleSignOut } = useAccountStore()
 <template>
   <div class="flex flex-row xl:gap-10 gap-2 md:gap-4 justify-center items-center xl:py-4 px-4 py-2 bg-second sticky top-0 z-10">
     <span class="text-xs xl:text-lg font-bold text-zinc-100">Welcome back, {{ account }}</span>
-    <StarpxButton @click="handleSignOut" class="!w-24 text-xs xl:text-lg font-bold" label="Sign out" />
+    <StarpxButton @click="handleSignOut" class="!w-28 text-xs xl:text-lg font-bold" label="Sign out" />
   </div>
 </template>
