0d8b15510c4189f21709faaba40679d9ebfceedd
Thanh Ho Duc (DEV) <thanhhd@aladintech.co>
2024-06-04 13:39:36 +0700

    fix exprired token

--- src/services/services.ts
+++ src/services/services.ts
@@ -7,8 +7,17 @@ class GqlService {
     try {
       const client = await gqlClient()
       return await client.graphql({ query, variables })
-    } catch (error) {
-      this.accountStore.handleSignOut()
+    } catch (err: any) {
+      if (err.errors && err.errors[0]) {
+        const errorCode = err.errors[0].message
+        if (errorCode === 'Unauthorized' || errorCode === 'TokenExpiredException') {
+          this.accountStore.handleSignOut()
+        } else {
+          // console.log('Other GraphQL Error:', err.errors[0])
+        }
+      } else {
+        this.accountStore.handleSignOut()
+      }
     }
   }
 }
